18c410cdf7fc47e69c6d296903ba6f792028d133
iamflowz <dakota.lewallen17@gmail.com>
2020-06-13 01:08:28 -0400

    handling form values

--- components/cards/contact.js
+++ components/cards/contact.js
@@ -1,47 +1,85 @@
-import { Grid, Typography, CardContent, TextField } from "@material-ui/core/";
+import { useState } from "react";
+import { Grid, Typography, Button, TextField } from "@material-ui/core/";
+import SendIcon from "@material-ui/icons/Send";
 import MyCard from "./card";
 
-const Contact = () => (
-  <MyCard>
-    <Grid container direction="column">
-      <Typography style={{ marginLeft: "1rem" }} variant="h3">
-        Contact
-      </Typography>
-      <Typography style={{ marginLeft: "1rem" }}>
-        If you would like to contact me but don't have social media, feel free
-        to leave your info and a message below.
-      </Typography>
-      <div style={{ padding: "1rem" }}>
-        <Grid item xs={12}>
-          <TextField
-            required
-            label="Name"
-            variant="filled"
-            style={{ width: "100%" }}
-          ></TextField>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            label="Email"
-            variant="filled"
-            style={{ width: "100%" }}
-          ></TextField>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            label="Message"
-            variant="filled"
-            multiline
-            rows={3}
-            size={"medium"}
-            style={{ width: "100%" }}
-          ></TextField>
-        </Grid>
-      </div>
-    </Grid>
-  </MyCard>
-);
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleChange = (event) => {
+    switch (event.target.id) {
+      case "name":
+        setName(event.target.value);
+        break;
+      case "email":
+        setEmail(event.target.value);
+        break;
+      case "msg":
+        setMessage(event.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <MyCard>
+      <Grid container direction="column">
+        <Typography style={{ marginLeft: "1rem" }} variant="h3">
+          Contact
+        </Typography>
+        <Typography style={{ marginLeft: "1rem" }}>
+          If you would like to contact me but don't have social media, feel free
+          to leave your info and a message below.
+        </Typography>
+        <div style={{ padding: "1rem" }}>
+          <Grid item xs={12}>
+            <TextField
+              id="name"
+              required
+              label="Name"
+              variant="filled"
+              style={{ width: "100%" }}
+              onChange={handleChange}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="email"
+              required
+              label="Email"
+              variant="filled"
+              style={{ width: "100%" }}
+              onChange={handleChange}
+            ></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="msg"
+              required
+              label="Message"
+              variant="filled"
+              multiline
+              rows={3}
+              size={"medium"}
+              style={{ width: "100%" }}
+              onChange={handleChange}
+            ></TextField>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginTop: "1rem" }}
+              onClick={() => {}}
+            >
+              Submit <SendIcon style={{ marginLeft: "0.5rem" }} />
+            </Button>
+          </Grid>
+        </div>
+      </Grid>
+    </MyCard>
+  );
+};
 
 export default Contact;
