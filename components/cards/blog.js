76964960b64f2eca61dd3be28439539795361277
iamflowz <dakota.lewallen17@gmail.com>
2020-06-12 22:58:30 -0400

    built out blog component

--- components/cards/blog.js
+++ components/cards/blog.js
@@ -1,13 +1,35 @@
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
+import {
+  ChatBubbleOutline,
+  InsertEmoticon,
+  PersonOutline,
+} from "@material-ui/icons/";
+import { Grid, Typography, CardContent } from "@material-ui/core/";
 import MyCard from "./card";
 
-const BlogCard = () => (
-  <MyCard>
-    <CardContent>
-      <Typography>Blog</Typography>
-    </CardContent>
-  </MyCard>
-);
+const BlogCard = ({ title, description, views, reactions, comments }) => {
+  return (
+    <MyCard>
+      <CardContent>
+        <Typography variant={"h3"}>{title}</Typography>
+        <Typography style={{ marginBottom: "0.5em" }}>{description}</Typography>
+        <Grid container>
+          <Grid item xs={12} lg={3} style={{ display: "flex" }}>
+            <PersonOutline style={{ marginRight: "0.5rem" }} />
+            <Typography>{views} Views</Typography>
+          </Grid>
+
+          <Grid item xs={12} lg={3} style={{ display: "flex" }}>
+            <InsertEmoticon style={{ marginRight: "0.5rem" }} />
+            <Typography>{reactions} Reactions</Typography>
+          </Grid>
+          <Grid item xs={12} lg={3} style={{ display: "flex" }}>
+            <ChatBubbleOutline style={{ marginRight: "0.5rem" }} />
+            <Typography>{comments} Comments</Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </MyCard>
+  );
+};
 
 export default BlogCard;
--- components/cards/intro.js
+++ components/cards/intro.js
@@ -1,28 +1,32 @@
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import MyCard from "./card";
 
 const Hero = () => (
-  <MyCard>
-    <CardContent>
-      {/* <img alt="my bust"></img> */}
-      <span
-        style={{
-          height: "10rem",
-          width: "10rem",
-          backgroundColor: "black",
-          display: "inline-block",
-          borderRadius: "1rem",
-        }}
-      ></span>
-      <Typography>Hi, I'm Dakota Lewallen.</Typography>
-      <Typography>
-        I’m here to bring simple solutions to your most challenging problems.
-        From initial concept all the way to automated deployments and
-        monitoring.{" "}
-      </Typography>
-    </CardContent>
-  </MyCard>
+  <Grid item xs={12}>
+    <Card>
+      <CardContent>
+        {/* <img alt="my bust"></img> */}
+        <span
+          style={{
+            height: "10rem",
+            width: "10rem",
+            backgroundColor: "black",
+            display: "inline-block",
+            borderRadius: "1rem",
+          }}
+        ></span>
+        <Typography>Hi, I'm Dakota Lewallen.</Typography>
+        <Typography>
+          I’m here to bring simple solutions to your most challenging problems.
+          From initial concept all the way to automated deployments and
+          monitoring.{" "}
+        </Typography>
+      </CardContent>
+    </Card>
+  </Grid>
 );
 
 export default Hero;
