691778d237b898c057f5a7c2d912d8afa1672311
iamflowz <dakota.lewallen17@gmail.com>
2020-06-12 23:25:23 -0400

    switch nav bar based on size

--- components/cards/intro.js
+++ components/cards/intro.js
@@ -2,7 +2,6 @@ import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import MyCard from "./card";
 
 const Hero = () => (
   <Grid item xs={12}>
@@ -22,7 +21,7 @@ const Hero = () => (
         <Typography>
           I’m here to bring simple solutions to your most challenging problems.
           From initial concept all the way to automated deployments and
-          monitoring.{" "}
+          monitoring.
         </Typography>
       </CardContent>
     </Card>
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,22 +1,33 @@
 import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 import styles from "./layout.module.css";
 import Logo from "./Logo32.svg";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    width: "100%",
+    position: "fixed",
+    top: 0,
+    [theme.breakpoints.down("sm")]: {
+      top: "unset",
+      bottom: 0,
+    },
+  },
+  topContainer: {
+    display: "flex",
+    marginTop: "5rem",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "2rem",
+    },
+  },
+}));
 
 const Layout = (props) => {
+  const classes = useStyles();
   return (
-    <div
-      className={styles.container}
-      style={{ display: "flex", marginTop: "2rem" }}
-    >
+    <div className={classes.topContainer}>
       {props.children}
-      <footer
-        style={{
-          width: "100%",
-          position: "fixed",
-          // left: "50vw",
-          bottom: 0,
-        }}
-      >
+      <footer className={classes.footer}>
         <BottomNavigation value="test" showLabels>
           <BottomNavigationAction label="Blog" value="blog" />
           <BottomNavigationAction label="Projects" value="projects" />
