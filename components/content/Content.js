2d2706310769831c310a9f199c625a112d87fc4c
iamflowz <dakota.lewallen17@gmail.com>
2020-06-14 10:38:48 -0400

    starting on wrappers

--- components/content/Content.js
+++ components/content/Content.js
@@ -1,66 +1,15 @@
 import Grid from "@material-ui/core/Grid";
 import Hero from "../cards/Hero";
-import BlogCard from "../cards/Blog";
+import BlogWrapper from "../wrappers/BlogWrapper";
+import ProjectWrapper from "../wrappers/ProjectWrapper";
 import Contact from "../cards/Contact";
-import Project from "../cards/Project";
 import Cert from "../cards/Certs";
 
-const Blog = () => (
-  <>
-    <BlogCard
-      title={"May 30th Side Project Check-in"}
-      description={
-        "Need some review or want to show off? Come share your side project with everyone! "
-      }
-      views={400}
-      reactions={64}
-      comments={24}
-    />
-    <BlogCard
-      title={"Best Tech Startup Podcasts?"}
-      description={"Share your favorite startup podcasts."}
-      views={55}
-      reactions={6}
-      comments={2}
-    />
-    <BlogCard
-      title={
-        "When starting a new project, when wouldn't you want to build in a cloud?"
-      }
-      description={
-        "Cloud providers are great, but when might you opt for rack-mounting or similar hosting for a project?"
-      }
-      views={578}
-      reactions={21}
-      comments={17}
-    />
-
-    <BlogCard
-      title={"May 21st Side Project Check-in"}
-      description={
-        "Need some review or want to show off? Come share your side project with everyone! "
-      }
-      views={400}
-      reactions={64}
-      comments={24}
-    />
-    <BlogCard
-      title={"deno-diceware. A Deno Password Generator"}
-      description={
-        "Check out this password generator I made... With Deno. *gasp*"
-      }
-      views={365}
-      reactions={25}
-      comments={1}
-    />
-  </>
-);
-
 const navs = {
-  blog: <Blog />,
+  blog: <BlogWrapper />,
   contact: <Contact />,
   certifications: <Cert />,
-  projects: <Project />,
+  projects: <ProjectWrapper />,
 };
 
 const Content = ({ children, navValue }) => {
---
+++ components/wrappers/ProjectWrapper.js
@@ -0,0 +1,10 @@
+import Project from "../cards/Project";
+
+const ProjectWrapper = () => (
+  <Project
+    title="dnd-graphql"
+    description="Graphql service wrapper for the Dungeons and Dragons 5e SRD"
+  />
+);
+
+export default ProjectWrapper;
