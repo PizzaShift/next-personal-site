d298127a2c25c1c3414a5294616f509ca5310308
Dakota Lewallen <dakota.lewallen17@gmail.com>
2020-08-08 11:51:42 -0400

    wrapping in a catch

--- pages/_document.js
+++ pages/_document.js
@@ -39,13 +39,17 @@ class MyDocument extends Document {
           <script async defer data-domain="dakotalewallen.me" src="https://plausible.io/js/plausible.js">
           </script>
           <script>
-            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
-            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-            // Insert Twitter Pixel ID and Standard Event data below
-            twq('init','o4c0c');
-            twq('track','PageView');
-            </script>
+            try {
+              !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+              },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+              a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+              // Insert Twitter Pixel ID and Standard Event data below
+              twq('init','o4c0c');
+              twq('track','PageView');
+            } catch(err) {
+              console.error("Couldn't start twitter pixel", err)
+            }
+          </script>
         </Head>
         <body>
           <Main />
