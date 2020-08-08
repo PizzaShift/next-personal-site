7f2e4d0e40f9550950124ff64a4727b6ccbb9cf3
Dakota Lewallen <dakota.lewallen17@gmail.com>
2020-08-08 11:45:06 -0400

    Cant have html comments

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,6 @@ class MyDocument extends Document {
           </style>
           <script async defer data-domain="dakotalewallen.me" src="https://plausible.io/js/plausible.js">
           </script>
-          <!-- Twitter universal website tag code -->
           <script>
             !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
             },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
@@ -47,7 +46,6 @@ class MyDocument extends Document {
             twq('init','o4c0c');
             twq('track','PageView');
             </script>
-            <!-- End Twitter universal website tag code -->
         </Head>
         <body>
           <Main />
