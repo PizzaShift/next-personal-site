8ee0ad397916829049b378be9d5f6338557696c7
iamflowz <dakota.lewallen17@gmail.com>
2020-08-15 19:39:21 -0400

    Adding to other pages

--- pages/certifications.js
+++ pages/certifications.js
@@ -1,10 +1,22 @@
+import { NextSeo } from "next-seo";
 import Certs from "../components/cards/Certs";
 import Root from "../components/root";
 
 const App = () => (
-  <Root navLocation="/certifications">
-    <Certs />
-  </Root>
+  <>
+    <NextSeo
+      title="Dakota Lewallen - Certifications"
+      description="Dakota Lewallens Certifications Page"
+      twitter={{
+        handle: "@fastflowz",
+        site: "@fastflowz",
+        cardType: "summary",
+      }}
+    />
+    <Root navLocation="/certifications">
+      <Certs />
+    </Root>
+  </>
 );
 
 export default App;
